44a95e322b1326bb01fff64371a59aac37d35cba
samaypatel27 <161898632+samaypatel27@users.noreply.github.com>
2025-01-24 11:30:19 -0500

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -68,65 +68,9 @@ struct ContentView: View {
                 }).padding()
             }
             
-                /*Button(action: {
-                    print("Do some Action")
-                    //ourText = " " + yourName
-                }, label: {
-                NavigationLink: destination: HumanitiesView()) {Text("Open View")
-                    //Spacer()
-                    //).frame(width: 100)
-                }
-                }
-                } */   
-                    
-                    /*HStack{
-                        Button("Environment"){
-                            print("Do some Action")
-                            ourText = " " + yourName
-                        }.padding(.all, 8.0).overlay(RoundedRectangle(cornerRadius: 16).stroke(.red,lineWidth: 5)).bold()
-                        //Spacer()
-                        Image("Trees").resizable().scaledToFit().frame(width: 100)
-                        
-                    }*/
-                    /*HStack{
-                        Button("Animals  "){
-                            print("Do some Action")
-                            ourText = " " + yourName
-                        }.padding(.all, 8.0).overlay(RoundedRectangle(cornerRadius: 16).stroke(.red,lineWidth: 5)).bold()
-                        //Spacer()
-                        Image("IMG_0007").resizable().scaledToFit().frame(width: 100)
-                    }*/
-                    
-                    /*HStack{
-                        Button("Education System"){
-                            print("Do some Action")
-                            ourText = " " + yourName
-                        }.padding(.all, 8.0).overlay(RoundedRectangle(cornerRadius: 16).stroke(.red,lineWidth: 5)).bold()
-                        //Spacer()
-                        Image("Image Asset").resizable().scaledToFit().frame(width: 100)
-                    }*/
+               
                 }
         }
     }
 }
-/*NavigationView{
-    VStack {
-        Spacer()
-        Image("logo")
-            .imageScale(.large)
-            .foregroundColor(.accentColor)
-        NavigationLink(
-            destination: HomeView(),
-            label:{
-                Text("Home")
-            })
-        NavigationLink(
-            destination: AboutView(),
-            label:{
-                Text("About Us")
-            })
-        NavigationLink(
-            destination: ContactView(),
-            label:{
-                Text("Contact Us")
-                })*/
+
